30f668f54b420358b6277a9c961c1710bd8f5485
박망키 <84365409+qkrtnfks128@users.noreply.github.com>
2022-08-16 20:13:01 +0900

    toJson

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:instagram_copy/css/theme.dart';
 import 'package:instagram_copy/page/home.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 //스크롤 다룰때
 import 'package:flutter/rendering.dart';
 import 'package:instagram_copy/page/upload.dart';
+
 //사진 가져오기
 import 'package:image_picker/image_picker.dart';
+
 //파일 다루는 함수가 들어있는 패키지
 import 'dart:io';
 
@@ -29,27 +32,29 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ///현재 탭의 상태
-  int tab=0;
-  late List _showList=[];
+  int tab = 0;
+  late List _showList = [];
   var userImage;
 
   getData() async {
-    var result = await http.get( Uri.parse('https://codingapple1.github.io/app/data.json') );
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
     if (result.statusCode == 200) {
       setState(() {
-        _showList=jsonDecode(result.body);
+        _showList = jsonDecode(result.body);
       });
-
     } else {
       throw Exception('실패함ㅅㄱ');
     }
-
   }
-addList(e){
+
+  addList(e) {
     setState(() {
+      print(e);
       _showList..addAll(e);
     });
-}
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -66,41 +71,59 @@ addList(e){
           title: const Text('Instagram'),
           actions: [
             IconButton(
-              onPressed: () async{
+              onPressed: () async {
                 ///이미지 선택화면 띄우는 코드
                 var picker = ImagePicker();
-                XFile? image = await picker.pickImage(source: ImageSource.gallery);
+                XFile? image =
+                    await picker.pickImage(source: ImageSource.gallery);
+
                 ///이미지 경로 저장
-                if(image!=null){
+                if (image != null) {
                   setState(() {
-                    userImage=File(image.path);
+                    userImage = File(image.path);
                   });
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (c)=>Upload(userImage:userImage))
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (c) => Upload(
+                        userImage: userImage,
+                        getMoreList: (e) {
+                          addList(e);
+                        },
+                        lastIndex:_showList.length.toString()
+                      ),
+                    ),
                   );
                 }
-
-
               },
               icon: const Icon(Icons.add_box_outlined),
             ),
           ],
         ),
-        body:[Home(data:_showList,getMoreList: (e){addList(e);}),Container(child:Text('샵'))][tab],
+        body: [
+          Home(
+              data: _showList,
+              getMoreList: (e) {
+                addList(e);
+              }),
+          Container(child: Text('샵'))
+        ][tab],
         bottomNavigationBar: BottomNavigationBar(
           showSelectedLabels: false,
           showUnselectedLabels: false,
           selectedItemColor: Colors.black87,
-          onTap: (i){
+          onTap: (i) {
             setState(() {
-              tab=i;
+              tab = i;
             });
           },
           items: const [
             BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined), label: '홈',tooltip: ''),
+                icon: Icon(Icons.home_outlined), label: '홈', tooltip: ''),
             BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_bag_outlined), label: '샵',tooltip: ''),
+                icon: Icon(Icons.shopping_bag_outlined),
+                label: '샵',
+                tooltip: ''),
           ],
         ),
       ),
--- lib/page/upload.dart
+++ lib/page/upload.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Upload extends StatefulWidget {
-   Upload({Key? key, required this.userImage}) : super(key: key);
+  Upload(
+      {Key? key,
+      required this.userImage,
+      required this.getMoreList,
+      required this.lastIndex})
+      : super(key: key);
+
   final userImage;
+  final Function(List) getMoreList;
+  final String lastIndex;
 
   @override
   State<Upload> createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
-  final TextEditingController inputController=TextEditingController();
-@override
+  final TextEditingController inputController = TextEditingController();
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    inputController.text='';
+    inputController.text = '';
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +43,17 @@ class _UploadState extends State<Upload> {
             IconButton(
               onPressed: () {
                 print('저장하기');
+                UploadValue addList = UploadValue(
+                    widget.lastIndex,
+                    widget.userImage,
+                    0,
+                    'May 5',
+                    inputController.text,
+                    false,
+                    '망키');
+
+                print(addList.toJson());
+                // widget.getMoreList(_showMoreList);
               },
               icon: Icon(
                 Icons.send,
@@ -43,30 +64,62 @@ class _UploadState extends State<Upload> {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0,vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
-                    width: double.infinity,height:300,
-                    child: Image.file(widget.userImage,fit: BoxFit.cover)),
-                SizedBox(height:20),
+                    width: double.infinity,
+                    height: 300,
+                    child: Image.file(widget.userImage, fit: BoxFit.cover)),
+                SizedBox(height: 20),
                 TextFormField(
                   controller: inputController,
-                  onChanged: (text){
+                  onChanged: (text) {
                     setState(() {
-                      inputController.text=text;
+                      inputController.text = text;
                     });
                   },
-                  decoration:InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),borderSide: BorderSide(color:Colors.grey)
-                    )
-                  ) ,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          borderSide: BorderSide(color: Colors.grey))),
                 ),
               ],
             ),
           ),
         ));
   }
+}
+
+class UploadValue {
+  String id;
+  dynamic image;
+  int likes;
+  String date;
+  String content;
+  bool liked;
+  String user;
+
+  UploadValue(this.id, this.image, this.likes, this.date, this.content,
+      this.liked, this.user);
+
+  UploadValue.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        image = json['image'],
+        likes = json['likes'],
+        date = json['date'],
+        content = json['content'],
+        liked = json['liked'],
+        user = json['user'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'image': image,
+        'likes': likes,
+        'date': date,
+        'content': content,
+        'liked': liked,
+        'user': user,
+      };
 }
